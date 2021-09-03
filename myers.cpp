ef307710fe964a74394aafba49a9ca0b278f6454
congtrung6391 <congtrung6391@gmail.com>
2021-09-03 23:15:36 +0700

    initial

--- myers.cpp
+++ myers.cpp
@@ -1,13 +1,18 @@
 int chunk (int a) {
 
+  if (a == 2) {
+    cout << 1;
+    return null;
+  }
+
   int x = 2;
 
-  if (a == 1) 
+  if (a != 1) 
   {
-    return 1;
-  }
-  else {
     cout << 1;
-  }
     return null;
+  }
+  else 
+    return 1;
+
 }
